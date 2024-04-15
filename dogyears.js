d152b11f4ec3f211ed0236495edacbd8abd64dbb
Umwizer <ruthumwizerwa@gmail.com>
2024-04-15 11:12:49 +0200

    Age Years in dogyears

---
+++ dogyears.js
@@ -0,0 +1,21 @@
+let myAge=20;
+// declared variable called my myAge and assigned value  as 20;
+let earlyAges=2;
+// declared variable as earlyAges and assigned value as 2 and used let because value can change.
+earlyAges*=10.5;
+let lateryears=myAge-2;
+// declared variable as lateryears and value of lateryears will be myAge-2;
+lateryears*=4;
+//to get my lateryears in dogYears is multiplied lateryears by 4;
+console.log(earlyAges);
+console.log(lateryears);
+let myAgeInDogYears=earlyAges+lateryears;
+// get myAgeInDogYears first declared variable called myAgeInDogYears and take earlyAges multiply with lateryears
+var myName="RUTH".toLowerCase();
+// declared variable called myName to store myname and also converted it into lowercase.
+console.log(myName);
+console.log(`myName is ${myName} . I am ${myAgeInDogYears} years old in dog years `);
+myAge=30;
+earlyAges=earlyAges * 10.5;
+console.log(myAge);
+console.log(earlyAges);
